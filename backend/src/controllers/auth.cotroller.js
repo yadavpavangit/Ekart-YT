24473cb8c1a0fa722e3795c021d0f3225e908897
yadavpavangit <yadavp07.07@gmail.com>
2026-05-01 20:04:14 +0530

    add functionality of verification and reverification of email while registration

--- backend/src/controllers/auth.cotroller.js
+++ backend/src/controllers/auth.cotroller.js
@@ -32,4 +32,62 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const verificationEmail = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Authorization token missing or invalid" });
+    }
+    const token = authHeader.split(" ")[1];
+    // TOKEN VERIFICATION
+    let decoded;
+    try {
+      decoded = jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Registration token has expired" });
+      }
+      return res.status(400).json({ message: "Token verification failed" });
+    }
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.token = null;
+    user.isVerified = true;
+    await user.save();
+    return res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const reverificationEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    }
+
+    const token = user.generateToken();
+    sendVerificationEmail(token, email);
+    user.token = token;
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: "Verification email sent successfully", user: user });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, verificationEmail, reverificationEmail };
